4ec73acf35c8235f5fd6c34d95694f773abcb0a8
SanchezEzequiel <kutarinon402@gmail.com>
2025-09-01 13:00:58 -0300

    carpeta eliminada

--- Ejercicio4/ejercicio4.js
+++
@@ -1,9 +0,0 @@
-function mostrarDatos() {
-    // Obtener elementos input
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let libreta = document.getElementById("libreta").value;
-    
-    // Mostrar datos en alert
-    alert("Los datos ingresados son: Nombre: " + nombre + " Apellido: " + apellido + " Libreta Universitaria: " + libreta);
-}
