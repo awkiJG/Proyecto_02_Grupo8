78412e384400b504b52f50119340148e01538ac5
SanchezEzequiel <kutarinon402@gmail.com>
2025-09-01 11:36:03 -0300

    comentarios agregados

--- Ejercicio2/ejercicio2.js
+++ Ejercicio2/ejercicio2.js
@@ -1,12 +1,15 @@
 
-function calcularPromedio(numero1, numero2, numero3) {
+// funcion para calcular el promedio de 3 numeros
+function calcularPromedio(numero1, numero2, numero3) { 
 
-    let promedio = (numero1 + numero2 + numero3) /3; 
+    let promedio = (numero1 + numero2 + numero3) /3; //promedio
     return promedio;
 }
 
+// boton calcular
 document.getElementById("calcular").onclick = function() {
 
+    //pedir 3 numeros al usuario con prompt
 numero1 = parseInt(prompt("Ingresa el primer numero:"));
 numero2 = parseInt(prompt("Ingresa el segundo numero:"));
 numero3 = parseInt(prompt("Ingresa el tercer numero:"));
@@ -14,6 +17,7 @@ numero3 = parseInt(prompt("Ingresa el tercer numero:"));
 
     let resultado = calcularPromedio(numero1, numero2, numero3);
 
+    // muestra el resultado
     document.getElementById("resultado").innerHTML = 
         "El promedio de los tres numeros es: " + resultado;
 };
